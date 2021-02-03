e151cd300e43a37bde697a30d0ea8d89ce1a41a4
Siyuan Zhou <siyuan.zhou@mongodb.com>
2021-02-03 23:40:21 +0000

    add indexed-insert workload.

--- generate.py
+++ generate.py
@@ -3,11 +3,13 @@ import jinja2
 
 templateLoader = jinja2.FileSystemLoader(searchpath="./")
 templateEnv = jinja2.Environment(loader=templateLoader, extensions=["jinja2.ext.loopcontrols"])
-TEMPLATE_FILE = "template.yml.j2"
+# TEMPLATE_FILE = "template.yml.j2"
+TEMPLATE_FILE = "indexed-insert.yml.j2"
 template = templateEnv.get_template(TEMPLATE_FILE)
 
 threads = [1, 16, 32, 64, 128, 256, 512, 768, 1024]
 threads = [1, 16, 64, 256, 1024, 2048]
+# threads = [1]
 iteration = 1
-s = template.render(threads = threads, total_phase = len(threads) * iteration * 2, iteration = iteration)
+s = template.render(threads = threads, iteration = iteration)
 print(s)
