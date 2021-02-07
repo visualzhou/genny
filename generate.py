75d0f20b7270eb33d8f294bfb81fc6332ed1902f
Siyuan Zhou <siyuan.zhou@mongodb.com>
2021-02-07 00:08:31 +0000

    Before moving to 7 nodes.

--- generate.py
+++ generate.py
@@ -4,12 +4,15 @@ import jinja2
 templateLoader = jinja2.FileSystemLoader(searchpath="./")
 templateEnv = jinja2.Environment(loader=templateLoader, extensions=["jinja2.ext.loopcontrols"])
 # TEMPLATE_FILE = "template.yml.j2"
-TEMPLATE_FILE = "indexed-insert.yml.j2"
+# TEMPLATE_FILE = "indexed-insert.yml.j2"
+# TEMPLATE_FILE = "indexed-update.yml.j2"
+# TEMPLATE_FILE = "query.yml.j2"
+TEMPLATE_FILE = "read-update.yml.j2"
 template = templateEnv.get_template(TEMPLATE_FILE)
 
 threads = [1, 16, 32, 64, 128, 256, 512, 768, 1024]
 threads = [1, 16, 64, 256, 1024, 2048]
-# threads = [1]
+threads = [1, 8, 32, 128, 512, 1024]
 iteration = 1
 s = template.render(threads = threads, iteration = iteration)
 print(s)
